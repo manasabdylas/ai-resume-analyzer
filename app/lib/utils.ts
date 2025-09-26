44e2e51c01bbce7844dbd3910772b5eb7a553220
Manas <manas.abdylas@gmail.com>
2025-09-26 10:00:54 +0800

    Resume feedback components

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,19 +1,21 @@
-/**
- * Formats a file size in bytes to a human-readable string (KB, MB, GB)
- * @param bytes - the size in bytes
- * @returns A formatted string with the appropiate unit
- */
+import {type ClassValue, clsx} from "clsx";
+import {twMerge} from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs))
+}
+
 export function formatSize(bytes: number): string {
-    if (bytes === 0 ) return '0 bytes';
+    if (bytes === 0) return '0 Bytes';
 
     const k = 1024;
     const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
 
-    // Determine the appropiate unit by calculating the log
+    // Determine the appropriate unit by calculating the log
     const i = Math.floor(Math.log(bytes) / Math.log(k));
 
     // Format with 2 decimal places and round
-    return parseFloat((bytes / Math.pow(k,i)).toFixed(2)) + ' ' + sizes[i];
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
 }
 
 export const generateUUID = () => crypto.randomUUID();
